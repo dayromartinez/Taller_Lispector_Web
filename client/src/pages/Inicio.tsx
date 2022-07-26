7a70194611f86cad3fff07daa0db2bb89c8ce5e4
Dayro Martínez <dayro.martinez@sofka.com.co>
2022-07-26 00:00:19 -0500

    Implementación del diseño en la página de Inicio

--- client/src/pages/Inicio.tsx
+++ client/src/pages/Inicio.tsx
@@ -4,12 +4,12 @@ import imagenClarice from './../images/Clarice1.jpg'
 import '.././index.css'
 import { dataState } from '../redux/reducers'
 import { PublicLayout } from '../layouts/PublicLayout';
-import { Alert, Snackbar } from '@mui/material'
+import { Alert, Box, Container, Snackbar } from '@mui/material'
 import { AuthLayout } from '../layouts/AuthLayout';
+import imagenEncabezado from '../images/Clarice_Pagina_Inicio_Encabezado_Final.png';
 
 
 export const InicioPage = () => {
-    const usuario = useSelector((state : dataState) => state.usuario);
 
     const [open, setOpen] = React.useState(false);
 
@@ -37,13 +37,17 @@ export const InicioPage = () => {
                     Sesión iniciada exitosamente. ¡Bienvenid@ al parche Taller Lispector!
                 </Alert>
             </Snackbar>
-            <div className='contenedor_frase_lispector'>
-                <img src={imagenClarice} className="imgClarice"/>
-                <div className='nombre_frase_lispector'>
-                    <h5 className='text-2xl my-7 mr-2 italic'>«Nací dura, heróica, solitaria y de pié».</h5>
-                    <h5 className='libro_frase'>Agua Viva (1975)</h5>
-                </div>
-            </div>
+            <Box>
+                <Box >
+                    
+                </Box>
+
+                <div className='bg-container-home' />
+                    
+
+            </Box>
+
+            {/* <img src={imagenEncabezado} />  */}
             
         </AuthLayout>
     )
