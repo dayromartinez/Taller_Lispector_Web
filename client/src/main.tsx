a42737a6b4a043d230b90187928c822e18d748e9
Dayro Martínez <dayro.martinez@sofka.com.co>
2022-07-28 23:47:21 -0500

    Refactor deploy vercel

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import store from './redux/store/index';
-import ThemeAppProvider from './styles/themeAppProvider';
 
 render(
   <Provider store={store}>
