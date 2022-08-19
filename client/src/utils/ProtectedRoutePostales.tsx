3523ad0b04d0bcb0d5b1a9b1ea9d2986d229ffca
daviddiazh <daviddiazlds1221@gmail.com>
2022-08-19 00:14:59 -0500

    Latest changes

--- client/src/utils/ProtectedRoutePostales.tsx
+++ client/src/utils/ProtectedRoutePostales.tsx
@@ -8,19 +8,23 @@ export const ProtectedRoutePostales = ({ children }) => {
     // const [match, setMatch] = useState(false)
     const user = useSelector(( state : dataState ) => state.usuario);
 
-    //if( user?.['uid'] !== undefined ){
+    if( user?.['uid'] !== undefined ){
         // user?.['publicationsCode']?.forEach(({publicacion}) => {
         //     if( publicacion.includes('El tiempo en que no nos vimos' )){
         //         setMatch(true);
         //     }
         // });
 
-        // user?.['publicationsCode']?.[0]?.['publicacion'] === 'El tiempo en que no nos vimos' ? setMatch(true) : setMatch(false)
-    //}
-
-    if( user?.['publicationsCode']?.[0]?.['publicacion'] !== 'El tiempo en que no nos vimos' || user?.['role'] !== 'admin' ){
-        return <Navigate to='/' />
+        if( user?.['publicationsCode']?.[0]?.['publicacion'] !== 'El tiempo en que no nos vimos' && user?.['role'] !== 'admin' ){
+            return <Navigate to='/' />
+        }
     }
+
+
+
+    // if( user?.['publicationsCode']?.[0]?.['publicacion'] !== 'El tiempo en que no nos vimos' && user?.['role'] !== 'admin' ){
+    //     return <Navigate to='/' />
+    // }
 
     return children;
 }
