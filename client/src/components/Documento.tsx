43011a7cdc30cf1d34f8d4ea04b84d500e14d784
Dayro Martínez <dayro.martinez@sofka.com.co>
2022-09-16 00:48:40 -0500

    Refactor componente Documento

--- client/src/components/Documento.tsx
+++ client/src/components/Documento.tsx
@@ -30,7 +30,7 @@ export const Documento = ({nombrePublicacion}) => {
     }
 
     useEffect(() => {
-        
+
         let PSPDFKit;
         selectPublicacion();
         (async function () {
@@ -53,36 +53,4 @@ export const Documento = ({nombrePublicacion}) => {
             />
         </div>
     );
-
-    
-    
-
-
-
-
-    // const canvasRef = useRef(null);
-	// useEffect(() => {
-	// 	(async function () {
-	// 		// We import this here so that it's only loaded during client-side rendering.
-	// 		const pdfJS = await import('pdfjs-dist/build/pdf');
-	// 		pdfJS.GlobalWorkerOptions.workerSrc =
-	// 			window.location.origin + '/pdf.worker.min.js';
-	// 		const pdf = await pdfJS.getDocument(escritoResistencia).promise;
-
-	// 		const page = await pdf.getPage(1);
-	// 		const viewport = page.getViewport({ scale: 1.5 });
-
-	// 		// Prepare canvas using PDF page dimensions.
-	// 		const canvas = canvasRef.current;
-	// 		const canvasContext = canvas.getContext('2d');
-	// 		canvas.height = viewport.height;
-	// 		canvas.width = viewport.width;
-
-	// 		// Render PDF page into canvas context.
-	// 		const renderContext = { canvasContext, viewport };
-	// 		page.render(renderContext);
-	// 	})();
-	// }, []);
-
-	// return <canvas ref={canvasRef} style={{ height: '100vh' }} />;
 }
